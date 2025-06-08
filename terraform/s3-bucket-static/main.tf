308196cfa4bebf145b1a73202ccb07df642f4e16
Rosana Marques <rosanadeveloper@gmail.com>
2025-06-08 19:51:39 -0300

    fix: codepipeline and terraform

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -1,34 +1,26 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  
 }
 
 variable "bucket_name" {
-  type        = string
-  description = "Nome base para o bucket S3 (será prefixado com 'static-site-')"
+  type = string
 }
 
 resource "aws_s3_bucket" "static_site_bucket" {
   bucket = "static-site-${var.bucket_name}"
-  
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
   tags = {
     Name        = "Static Site Bucket"
     Environment = "Production"
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "static_site" {
-  bucket = aws_s3_bucket.static_site_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "404.html"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "static_site" {
+resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
 
   block_public_acls       = false
@@ -37,43 +29,19 @@ resource "aws_s3_bucket_public_access_block" "static_site" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_ownership_controls" "static_site" {
+resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
-  
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "static_site" {
+resource "aws_s3_bucket_acl" "static_site_bucket" {
   depends_on = [
-    aws_s3_bucket_public_access_block.static_site,
-    aws_s3_bucket_ownership_controls.static_site
+	aws_s3_bucket_public_access_block.static_site_bucket,
+	aws_s3_bucket_ownership_controls.static_site_bucket,
   ]
 
   bucket = aws_s3_bucket.static_site_bucket.id
   acl    = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "static_site_policy" {
-  depends_on = [aws_s3_bucket_acl.static_site]
-  
-  bucket = aws_s3_bucket.static_site_bucket.id
-  policy = data.aws_iam_policy_document.static_site_policy.json
-}
-
-data "aws_iam_policy_document" "static_site_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.static_site_bucket.arn}/*"]
-    
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-  }
-}
-
-output "website_url" {
-  value = aws_s3_bucket_website_configuration.static_site.website_endpoint
 }
