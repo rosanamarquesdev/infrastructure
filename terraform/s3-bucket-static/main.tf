20250152b08b3ba4e3be5ae92413a3800e850008
Rosana Marques <rosanadeveloper@gmail.com>
2025-06-08 23:44:33 -0300

    fix: add depends_on

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -87,8 +87,11 @@ resource "aws_s3_bucket_acl" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
   acl    = "public-read"
 }
-
 resource "aws_s3_bucket_policy" "public_policy" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.static_site_bucket
+  ]
+
   bucket = aws_s3_bucket.static_site_bucket.id
 
   policy = jsonencode({
