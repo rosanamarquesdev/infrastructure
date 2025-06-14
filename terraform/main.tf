bfadbb26d5d1c33f610affef3001a0fda414e3cb
Rosana Marques <rosanadeveloper@gmail.com>
2025-06-14 20:56:38 -0300

    fix: variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "bucket_name" {
-  type = string
-}
-
 module "static_site" {
   source      = "./s3-bucket-static"
   bucket_name = var.bucket_name
