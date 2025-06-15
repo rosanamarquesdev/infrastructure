c563b747f5ec46314f29e23fef91dc9c4b4e697d
Rosana Marques <rosanadeveloper@gmail.com>
2025-06-15 15:45:29 -0300

    fix: add publish=true

--- terraform/infra-core/main.tf
+++ terraform/infra-core/main.tf
@@ -69,7 +69,7 @@
     filename      = "${path.module}/lambda/lambda.zip"
     memory_size   = 128
     timeout       = 1
-
+    publish       = true
     vpc_config {
         subnet_ids         = [aws_subnet.private.id]
         security_group_ids = [aws_security_group.lambda_sg.id]
