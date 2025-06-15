9c5374a3832c8776ad9152f2c977d00cb5dba18d
Rosana Marques <rosanadeveloper@gmail.com>
2025-06-15 16:21:01 -0300

    fix: lifecycle

--- terraform/infra-core/main.tf
+++ terraform/infra-core/main.tf
@@ -69,10 +69,14 @@
     filename      = "${path.module}/lambda/lambda.zip"
     memory_size   = 128
     timeout       = 1
-    publish       = true
     vpc_config {
         subnet_ids         = [aws_subnet.private.id]
         security_group_ids = [aws_security_group.lambda_sg.id]
+    }
+    lifecycle {
+        ignore_changes = [
+            code_signing_config_arn
+        ]
     }
 
     depends_on = [
